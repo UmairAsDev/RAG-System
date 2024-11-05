df457a472ea7456469022563e142fc9387e031a7
UmairAsDev <167744708+UmairAsDev@users.noreply.github.com>
2024-11-05 16:35:43 +0500

    embed

--- src/llm_embedings.py
+++ src/llm_embedings.py
@@ -1,8 +1,15 @@
 from sentence_transformers import SentenceTransformer
-from transformers import DistilBertModel
-import torch
+# from transformers import DistilBertModel
+# import torch
 embed_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
-model = DistilBertModel("distilbert-base-uncased", torch_dtype=torch.float16, attn_implementation = "eager")
+# model = DistilBertModel.from_pretrained("distilbert-base-uncased", torch_dtype=torch.float16, attn_implementation="eager")
+
+sentences = ["This is an example sentence", "Each sentence is converted"]
+
+embedings = embed_model.encode(sentences)
+
+print(embedings)
+
 
 
