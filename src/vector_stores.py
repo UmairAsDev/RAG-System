f36ca29c86115acad7e9064e57027251600dcb4b
UmairAsDev <167744708+UmairAsDev@users.noreply.github.com>
2024-11-05 20:15:24 +0500

    main.py

--- src/vector_stores.py
+++ src/vector_stores.py
@@ -2,18 +2,24 @@ from qdrant_client import QdrantClient
 from llm_embedings import llm_embeddings
 import yaml
 
-with open(r'config\config.yaml', 'r') as file:
-    secrets = yaml.safe_load(file)
+def get_vector_store():
+    try:
+        with open(r'config\config.yaml', 'r') as file:
+            secrets = yaml.safe_load(file)
 
-qdrant_url = secrets["QDRANT_URL"]
-qdrant_api = secrets["QDRANT_API"]
-collection_name = "Multi_Rag_System"
+        qdrant_url = secrets["QDRANT_URL"]
+        qdrant_api = secrets["QDRANT_API"]
+        collection_name = "Multi_Rag_System"
 
-qdrant_client = QdrantClient(
-    embed_model = llm_embeddings,
-    url = qdrant_url,
-    prefer_grpc=True,
-    api_key=qdrant_api,
-    collection_name = collection_name,
-)
+        qdrant_client = QdrantClient(
+            embed_model = llm_embeddings,
+            url = qdrant_url,
+            prefer_grpc=True,
+            api_key=qdrant_api,
+            collection_name = collection_name,
+        )
+        return qdrant_client
+    except Exception as e:
+        print(f"Vector store can't be created...{e}")
+        return None
 
