edb7c19c214ff9b30636088a1e9c6f465cd2f8b8
UmairAsDev <167744708+UmairAsDev@users.noreply.github.com>
2024-11-08 19:00:22 +0500

    new

--- src/llm_manager.py
+++ src/llm_manager.py
@@ -1,7 +1,3 @@
-from transformers import AutoModel
-from llm_embedings import llm_embeddings
-from vector_stores import get_vector_store
-def llm_manager(selected_model, llm_embeddings, get_vector_store):
-    if selected_model == "gpt-neo":
-        pass
-        
+from transformers import pipeline
+
+model_name  = ["gpt2","distilgpt2", "facebook/blenderbot-400M-distill", "microsoft/DialoGPT-medium", "t5-small"]
--- src/vector_stores.py
+++ src/vector_stores.py
@@ -1,8 +1,10 @@
 from langchain_qdrant import QdrantVectorStore
 from llm_embedings import llm_embeddings
 import yaml
+import distance
 
 def get_vector_store():
+    
     try:
         with open(r'config\config.yaml', 'r') as file:
             secrets = yaml.safe_load(file)
@@ -18,8 +20,17 @@ def get_vector_store():
             api_key=qdrant_api,
             collection_name = collection_name,
         )
+        
+        if not qdrant_client.has_collection(collection_name):
+            qdrant_client.create_collection(
+                collection_name=collection_name,
+                vectors_config={"size": llm_embeddings.get_dimension(), "distance": distance.COSINE}
+            )
+            
         return qdrant_client
     except Exception as e:
         print(f"Vector store can't be created...{e}")
         return None
 
+def add_documents(collection_name= "Multi_Rag_System"):
+    pass
