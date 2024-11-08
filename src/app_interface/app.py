17d3b7517e55cb8a2fc6b9ef836991da9ac54b91
UmairAsDev <167744708+UmairAsDev@users.noreply.github.com>
2024-11-08 18:15:15 +0500

    new structure

--- src/app_interface/app.py
+++ src/app_interface/app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from pages import home, data_upload, data_analysis, query_interface, model_selection, history, about
 
-# Dictionary for page mapping
+st.set_page_config("RAG-PRO")
+
 PAGES = {
     "Home": home,
     "Data Upload": data_upload,
@@ -12,7 +13,6 @@ PAGES = {
     "About": about
 }
 
-# Sidebar for Navigation
 st.sidebar.title("Navigation")
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 
--- src/llm_manager.py
+++ src/llm_manager.py
@@ -2,4 +2,6 @@ from transformers import AutoModel
 from llm_embedings import llm_embeddings
 from vector_stores import get_vector_store
 def llm_manager(selected_model, llm_embeddings, get_vector_store):
-    pass
+    if selected_model == "gpt-neo":
+        pass
+        
