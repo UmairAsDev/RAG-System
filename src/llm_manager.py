2585bbf126561fa5180af714f6ce2e413a1ed8a7
UmairAsDev <167744708+UmairAsDev@users.noreply.github.com>
2024-11-11 14:25:41 +0500

    func

--- src/llm_manager.py
+++ src/llm_manager.py
@@ -1,6 +1,14 @@
 from transformers import AutoModel
+from llm_embedings import llm_embeddings
+from vector_stores import get_vector_store
+from vector_stores import add_documents
 
-model_names  = ["gpt2","distilgpt2", "facebook/blenderbot-400M-distill", "microsoft/DialoGPT-medium", "t5-small"]
-generators = {model_name: AutoModel.from_pretrained(model_name) for model_name in model_names}
+llm_embedding = llm_embeddings()
 
-print(generators)
+def llm_manager(select_model, llm_embedding, vector_db):
+    select_model  = ["gpt2","distilgpt2", "facebook/blenderbot-400M-distill", "microsoft/DialoGPT-medium", "t5-small"]
+    generators = {model_name: AutoModel.from_pretrained(model_name) for model_name in select_model}
+    
+    
+
+
